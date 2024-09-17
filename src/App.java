e9ef0af4a20370e99ac4819574a374e7ea3ea839
ShahadSulais <shahad.ali.sulais@gmail.com>
2024-09-17 16:07:59 +0300

    shahad device

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Random function!");
         System.out.println("From device 2");
+        System.out.println("From device 2");
     }
 }
